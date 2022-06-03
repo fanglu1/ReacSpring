b655f9ddd92772d41d69fc99df36a777e9e6d199
Antony Lu <fanglu0805@gmail.com>
2022-06-03 11:03:43 -0400

    first

--- src/main/java/com/payroll/DatabaseLoader.java
+++ src/main/java/com/payroll/DatabaseLoader.java
@@ -2,20 +2,52 @@ package com.payroll;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DatabaseLoader implements CommandLineRunner {
 
     private final EmployeeRepository repository;
+    private final ManagerRepository managers;
+    private final EmployeeRepository employees;
+
 
     @Autowired
-    public DatabaseLoader(EmployeeRepository repository) {
+    public DatabaseLoader(EmployeeRepository repository, EmployeeRepository employeeRepository,
+                          ManagerRepository managerRepository) {
         this.repository = repository;
+
+        this.employees = employeeRepository;
+        this.managers = managerRepository;
     }
 
     @Override
     public void run(String... strings) throws Exception {
-        this.repository.save(new Employee("Frodo", "Baggins", "ring bearer"));
+
+        Manager greg = this.managers.save(new Manager("greg", "turnquist",
+                "ROLE_MANAGER"));
+        Manager oliver = this.managers.save(new Manager("oliver", "gierke",
+                "ROLE_MANAGER"));
+
+        SecurityContextHolder.getContext().setAuthentication(
+                new UsernamePasswordAuthenticationToken("greg", "doesn't matter",
+                        AuthorityUtils.createAuthorityList("ROLE_MANAGER")));
+
+        this.employees.save(new Employee("Frodo", "Baggins", "ring bearer", greg));
+        this.employees.save(new Employee("Bilbo", "Baggins", "burglar", greg));
+        this.employees.save(new Employee("Gandalf", "the Grey", "wizard", greg));
+
+        SecurityContextHolder.getContext().setAuthentication(
+                new UsernamePasswordAuthenticationToken("oliver", "doesn't matter",
+                        AuthorityUtils.createAuthorityList("ROLE_MANAGER")));
+
+        this.employees.save(new Employee("Samwise", "Gamgee", "gardener", oliver));
+        this.employees.save(new Employee("Merry", "Brandybuck", "pony rider", oliver));
+        this.employees.save(new Employee("Peregrin", "Took", "pipe smoker", oliver));
+
+        SecurityContextHolder.clearContext();
     }
 }
